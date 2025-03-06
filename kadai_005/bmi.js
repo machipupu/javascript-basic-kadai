ab1ac12dbf217ad78685d41d064344cead7f838e
machipupu <misa.0213@icloud.com>
2025-03-06 14:17:15 +0900

    課題を再アップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,7 +1,6 @@
 // 宣言+値の代入
-let userWeighe = 68;
+let userWeight = 68;
 let userHeight = 1.7;
-let bmi = 68 / (1.7 * 1.7);
 
 // コンソールの出力
-console.log(bmi)
+console.log(userWeight / (userHeight * userHeight))

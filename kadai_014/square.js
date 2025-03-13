da501079fdc6161f97ec5592f2f3e442e87a093d
machipupu <misa.0213@icloud.com>
2025-03-13 18:17:20 +0900

    課題をアップロード

---
+++ kadai_014/square.js
@@ -0,0 +1,8 @@
+// 値を二乗し、その結果を返す関数
+function square(num) {
+  return num * num; // 引数を二乗して戻り値として返す
+}
+
+// 関数を呼び出し、計算結果をコンソールに出力
+const result = square(10); // 引数に「10」を渡す
+console.log(result); // 計算結果（100）を表示
